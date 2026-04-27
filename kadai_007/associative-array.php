f92869ff98c1b30b0c407fb4cc1863b934af20d5
NYAmiz <mizu.ta.515@gmail.com>
2026-04-27 22:08:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>連想配列を作ってキーと値を出力しよう(課題)</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    //連想配列に値を代入
+    $personal_value = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($personal_value);
